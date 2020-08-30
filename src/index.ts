517fca5438174a6a23494f3bf1c79897ad70af8d
Luan Bresolla <luanpires34@gmail.com>
2020-08-29 23:14:51 -0300

    Adicinando variavel ambiente da porta da api

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server started in port 3333 🚀');
 });
